9ccd62c7844727b214434e91acc2875c56be9a74
Ritika Mehta <mehtaritika@gmail.com>
2024-03-14 19:58:31 -0400

    made changes

--- script/script.js
+++ script/script.js
@@ -1,1 +1,14 @@
-console.log(document.getElementByName(genre));
+const books = [
+    {title: "Book1", genre : "mystery"},
+    {title: "Book12", genre : "romance"}
+]
+  
+window.onload = pageLoad;
+function pageLoad(){
+    var formHandle = document.forms.bookForm;
+    document.getElementById("submit").onclick = check;
+    function check(){
+        console.log(document.getElementByName("genre").value);
+    }
+    return false;
+};
