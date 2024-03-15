636bb5d32db6f13dbdd1192ce188cc777400790c
Ritika Mehta <mehtaritika@gmail.com>
2024-03-14 21:28:46 -0400

    added forech function

--- script/script.js
+++ script/script.js
@@ -6,9 +6,20 @@ const books = [
 window.onload = pageLoad;
 function pageLoad(){
     var formHandle = document.forms.bookForm;
+    
     document.getElementById("submit").onclick = check;
-    function check(){
-        console.log(document.getElementByName("genre").value);
+    function check()
+    {
+        var genreInput = formHandle.genre.value;
+        // console.log(formHandle.genre.value);
+        // console.log(books[1].genre);
+        books.forEach(function(index){
+            // console.log(index.genre);
+            // console.log(genreInput);
+
+            if(index.genre === genreInput)
+            console.log(index.genre+" "+ genreInput);
+        });
     }
     return false;
 };
